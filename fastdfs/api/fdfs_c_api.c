4dcfd6edcf7d27a791a9950eda5152ff6c4c1370
hang-ubuntu <1477649956@qq.com>
2022-07-27 17:49:22 +0800

    资源整理

---
+++ fastdfs/api/fdfs_c_api.c
@@ -0,0 +1,70 @@
+/**
+* Copyright (C) 2008 Happy Fish / YuQing
+*
+* FastDFS may be copied only under the terms of the GNU General
+* Public License V3, which may be found in the FastDFS source kit.
+* Please visit the FastDFS Home Page http://www.fastken.com/ for more detail.
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "fdfs_client.h"
+#include "fdfs_c_api.h"
+
+int fdfs_upload_file(const char *conf_filename,const char *local_filename,char *filer_id)
+{
+	char group_name[FDFS_GROUP_NAME_MAX_LEN + 1];
+	ConnectionInfo *pTrackerServer;
+	int result;
+	int store_path_index;
+	ConnectionInfo storageServer;
+//	char file_id[128];
+	
+    // 初始化
+	if ((result=fdfs_client_init(conf_filename)) != 0)
+	{
+		return result;
+	}
+    // 从配置文件中获取tracker连接信息
+	pTrackerServer = tracker_get_connection();
+	if (pTrackerServer == NULL)
+	{
+		fdfs_client_destroy();
+		return errno != 0 ? errno : ECONNREFUSED;
+	}
+
+	*group_name = '\0';
+    // 获取存储节点信息
+	if ((result=tracker_query_storage_store(pTrackerServer, \
+	                &storageServer, group_name, &store_path_index)) != 0)
+	{
+		fdfs_client_destroy();
+		fprintf(stderr, "tracker_query_storage fail, " \
+			"error no: %d, error info: %s\n", \
+			result, STRERROR(result));
+		return result;
+	}
+    // 文件上传
+	result = storage_upload_by_filename1(pTrackerServer, \
+			&storageServer, store_path_index, \
+			local_filename, NULL, \
+			NULL, 0, group_name, filer_id);
+	if (result != 0)
+	{
+		fprintf(stderr, "upload file fail, " \
+			"error no: %d, error info: %s\n", \
+			result, STRERROR(result));
+	}
+
+	tracker_close_connection_ex(pTrackerServer, true);
+	fdfs_client_destroy();
+
+	return result;
+}
+
+
---
+++ fastdfs/api/fdfs_c_api.h
@@ -0,0 +1,4 @@
+#ifndef FDSF_C_API
+#define FDSF_C_API
+int fdfs_upload_file(const char *conf_filename, const char *local_filename,char *file_id);
+#endif
