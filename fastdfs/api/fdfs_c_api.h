6df59f087adba083255b8f1f4a058d32f2358d00
hang-ubuntu <1477649956@qq.com>
2022-08-03 22:32:02 +0800

    重启修整

--- fastdfs/api/fdfs_c_api.h
+++
@@ -1,5 +0,0 @@
-#ifndef FDSF_C_API
-#define FDSF_C_API
-int fdfs_upload_file(const char *conf_filename, const char *local_filename,char *file_id);
-void my_fdfs_upload_file(const char *conf_filename,const char *local_filename,char *file_id,int size);
-#endif
